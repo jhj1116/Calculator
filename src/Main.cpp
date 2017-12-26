3d355a837ff0c2a136a45b7ff6ba52f5571f413d
jhj1116 <34591085+jhj1116@users.noreply.github.com>
2017-12-27 01:59:47 +0900

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,31 +4,25 @@
 
 int main(int argc, char* argv[])
 {
-	Calculator* cal = (Calculator*)malloc(200);
-	Calculator* cal2 = (Calculator*)malloc(200);
-	Calculator* cal3 = (Calculator*)malloc(200);
-	Calculator* cal4 = (Calculator*)malloc(200);
-	Calculator* cal5 = (Calculator*)malloc(200);
-	Calculator* cal6 = (Calculator*)malloc(200);
-	cal = new Calculator("1+2");
+	Calculator* cal = new Calculator("1+2");
 	std::cout << cal->getValue() << std::endl;
 	
 	cal->setString("3*(2+1)");
 	std::cout << cal->getValue() << std::endl;
 
-	cal2 = new Calculator("2*4%2");
+	Calculator* cal2 = new Calculator("2*4%2");
 	std::cout << cal2->getValue() << std::endl;
 
-	cal3 = new Calculator("6%5*3");
+	Calculator* cal3 = new Calculator("6%5*3");
 	std::cout << cal3->getValue() << std::endl;
 
-	cal4 = new Calculator("3*sin(4*PI)");
+	Calculator* cal4 = new Calculator("3*sin(4*PI)");
 	std::cout << cal4->getValue() << std::endl;
 
-	cal5 = new Calculator("4*cos((2+1)*PI)");
+	Calculator* cal5 = new Calculator("4*cos((2+1)*PI)");
 	std::cout << cal5->getValue() << std::endl;
 
-	cal6 = new Calculator("3*cos(4)*sin(4)");
+	Calculator* cal6 = new Calculator("3*cos(4)*sin(4)");
 	std::cout << cal6->getValue() << std::endl;
 
 	delete cal;
